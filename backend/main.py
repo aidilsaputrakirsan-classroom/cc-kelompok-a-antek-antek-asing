aec2eaecee5dbd21e6f6dbc98e83e35e34240b98
Muhammad Athala Romero <10231059@student.itk.ac.id>
2026-04-05 20:29:00 +0800

    Backend Endpoint Updated

--- backend/main.py
+++ backend/main.py
@@ -118,7 +118,6 @@ def team_info():
     return {
         "team": "cloud-team-antek-antek-asing",
         "members": [
-            # TODO: Isi dengan data tim Anda
             {"name": "Muhammad Athala Romero", "nim": "10231059", "role": "Lead Backend"},
             {"name": "Muhammad Bagas Setiawan", "nim": "10231061", "role": "Lead Frontend"},
             {"name": "Muhammad Fikri Haikal Ariadma", "nim": "10231063", "role": "Lead DevOps"},
